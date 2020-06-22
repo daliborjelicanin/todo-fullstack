e19eb61a9669cf82d8682a18b5ee5365e108a51e
Dalibor Jelicanin <jelicanindalibor@gmail.com>
2020-06-22 16:24:34 +0200

    commit 3

--- todo-frontend/src/store/index.js
+++ todo-frontend/src/store/index.js
@@ -4,7 +4,7 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 // Vuex je centralizovano skladiste state-a nase aplikacije, kao npr. Redux
 // U ovom primeru imamo samo baseUrl, koji setujemo kako bismo iz cele aplikacije
-// znali na kojoj adresi nam je backend server
+// znali na kojoj adresi nam je backend server...
 export default new Vuex.Store({
   state: {
     baseUrl: 'http://localhost:80/todofullstack/todo-backend'
